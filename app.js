65b15c51a10440ffbdd1d5a242d6be9cfdc59c9a
Max <max@administrators-iMac.local>
2020-03-18 13:14:01 -0700

    updates

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ app.set('view engine', 'ejs');
 app.get("/", function (req, res) {
     
     var today = new Date();
-    var currentDay = today.getDay();
-    const daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    var day = daysWeek[currentDay];
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+    
+    // var currentDay = today.getDay();
+    // const daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    // var day = daysWeek[currentDay];
     res.render('list', { kindOfDay: day });
 
 });

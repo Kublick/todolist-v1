39fe86379bd6ae988e4edc17634dc0a019281b1a
Max Blancarte <blancarte@gmail.com>
2020-03-18 21:42:27 -0700

    updates to list handlling

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+var items = [];
+
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    
-    var today = new Date();
 
+    var today = new Date();
     var options = {
         weekday: "long",
         day: "numeric",
@@ -17,12 +21,18 @@ app.get("/", function (req, res) {
     };
 
     var day = today.toLocaleDateString("en-US", options);
-    
+
     // var currentDay = today.getDay();
     // const daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     // var day = daysWeek[currentDay];
-    res.render('list', { kindOfDay: day });
+    res.render('list', { kindOfDay: day, newListItems: items});
+    
+});
 
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function () {

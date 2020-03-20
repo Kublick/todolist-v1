0e213c7eae6fd478776c758e8a20bc31581724b7
max <blancarte@gmail.com>
2020-03-19 20:28:48 -0700

    additional fixes

--- app.js
+++ app.js
@@ -4,37 +4,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+var items = ["Eat Food"];
 
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
+  var day = today.toLocaleDateString("en-US", options);
 
-    var day = today.toLocaleDateString("en-US", options);
-
-    // var currentDay = today.getDay();
-    // const daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    // var day = daysWeek[currentDay];
-    res.render('list', { kindOfDay: day, newListItems: items});
-    
+  // var currentDay = today.getDay();
+  // const daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  // var day = daysWeek[currentDay];
+  res.render("list", { kindOfDay: day, newListItems: items });
 });
 
-app.post("/", function (req, res) {
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
-app.listen(3000, function () {
-    console.log("server is running")
+app.listen(3000, function() {
+  console.log("server is running");
 });

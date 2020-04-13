1b40099ef25828a929f16af7e8a6375270e476f5
Max <blancarte@gmail.com>
2020-04-12 23:19:03 -0700

    added a note

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
-
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-
-
-
 const app = express();
 
 var items = ["Eat Food"];
@@ -14,18 +10,17 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req,res) {
+app.get("/", function (req, res) {
   let day = date();
   res.render("list", { kindOfDay: day, newListItems: items });
- 
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
   var item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("server is running");
 });
--- date.js
+++ date.js
@@ -1,14 +1,16 @@
 
 module.exports = getDate;
 
+// This file needed something very important
+
 function getDate() {
-        let today = new Date();
-        let options = {
-          weekday: "long",
-          day: "numeric",
-          month: "long"
-        }
-    
-        let day = today.toLocaleDateString("en-US", options);
-        return day;
-    }
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+
+  let day = today.toLocaleDateString("en-US", options);
+  return day;
+}
